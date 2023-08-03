9ff2db85b979985e4acdbcbe4d93b8af1edde38e
Tony <economic_luojie@outlook.com>
2023-08-03 14:33:58 +0800

    feat: 组件化开发

---
+++ src/views/CommonViews.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'CommonViews',
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/duoji/siji.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    无限级菜单测试======四级页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Siji'
+}
+</script>
+
+<style scoped>
+
+</style>
