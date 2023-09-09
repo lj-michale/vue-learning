482c1737c75d6effd018df4823c796296509a621
Tony <economic_luojie@outlook.com>
2023-09-09 16:42:34 +0800

    feat: 首页数据卡片数据变化颜色样式调整

--- src/views/index/mainIndex.vue
+++ src/views/index/mainIndex.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="data-box">
     <h4>数据概览</h4>
-    <el-row :gutter="15">
-      <el-col :md="8" :xs="24">
+    <el-row :gutter="20">
+      <el-col :md="4" :xs="15">
         <el-card shadow="never" class="item-background">
           <div class="item">
             <div class="main">
@@ -19,7 +19,7 @@
           </div>
         </el-card>
       </el-col>
-      <el-col :md="8" :xs="24">
+      <el-col :md="4" :xs="15">
         <el-card shadow="never">
           <div class="item">
             <div class="main">
@@ -36,7 +36,7 @@
           </div>
         </el-card>
       </el-col>
-      <el-col :md="8" :xs="24">
+      <el-col :md="4" :xs="15">
         <el-card shadow="never">
           <div class="item">
             <div class="main">
@@ -50,7 +50,7 @@
           </div>
         </el-card>
       </el-col>
-      <el-col :md="8" :xs="24">
+      <el-col :md="4" :xs="15">
         <el-card shadow="never">
           <div class="item">
             <div class="main">
@@ -64,7 +64,7 @@
           </div>
         </el-card>
       </el-col>
-      <el-col :md="8" :xs="24">
+      <el-col :md="4" :xs="15">
         <el-card shadow="never">
           <div class="item">
             <div class="main">
@@ -90,168 +90,12 @@ import Card from '@/components/card/Card.vue'
 </script>
 
 <style lang="scss">
-  $top:top;
-  $bottom:bottom;
-  $left:left;
-  $right:right;
-  $leftright: ($left, $right);
-  $pinkk: #eec2d3;
-  $bluee: #409eff;
-  $yelloww: #f4d884;
-  $grennn: #89dda0;
-  $purplee: #78a2ea;
-  $lightBluee: #b8d6ff;
 
-  $list: bluee pinkk yelloww grennn purplee lightBluee;
-  $list1: $bluee $pinkk $yelloww $grennn $purplee $lightBluee;
-  %shadow{
-    background: #fff;
-    -webkit-box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.2);
-    box-shadow: 4px 4px 40px rgba(0, 0, 0, 0.2);
-    border-color: rgba(0, 0, 0, 0.2);
-    .title{
-      font-size: 14px;
-      padding: 10px;
-      i{
-        margin-#{$right}: 5px;
-      }
-    }
-  }
+.data-box .item p span.up {
+  color: #f56c6c;
+}
 
-  @mixin flex($direction:row,$content:space-between){
-    display: flex;
-    flex-direction: $direction;
-    justify-content: $content;
-  }
-  .card{
-    color: #666;
-    @extend %shadow;
-
-    ul{
-      @include flex;
-      li{
-        flex: 1;
-        a{
-          color: #666666;
-          align-items:center;
-          padding-#{$top}: 20px;
-          padding-#{$bottom}: 20px;
-          @include flex(column);
-          span{
-            height: 44px;
-          }
-          .num{
-            line-height: 44px;
-            font-size: 42px;
-            color: $bluee;
-            margin: 0px;
-          }
-        }
-        .kjfs-bluee{
-          color: $bluee;
-        }
-        .kjfs-pinkk{
-          color: $pinkk;
-        }
-        .kjfs-yelloww{
-          color: $yelloww;
-        }
-        .kjfs-grennn{
-          color: $grennn;
-        }
-        .kjfs-purplee{
-          color: $purplee;
-        }
-        .kjfs-lightBluee{
-          color: $lightBluee;
-        }
-        &:hover{
-          .kjfs-bluee{
-            color: #ffffff;
-            background: $bluee;
-          }
-          .kjfs-pinkk{
-            color: #ffffff;
-            background: $pinkk;
-          }
-          .kjfs-yelloww{
-            color: #ffffff;
-            background: $yelloww;
-          }
-          .kjfs-grennn{
-            color: #ffffff;
-            background: $grennn;
-          }
-          .kjfs-purplee{
-            color: #ffffff;
-            background: $purplee;
-          }
-          .kjfs-lightBluee{
-            color: #ffffff;
-            background: $lightBluee;
-          }
-        }
-      }
-    }
-    .table{
-      padding: 21px;
-      p{
-        height: 52px;
-        line-height: 52px;
-        border: 1px solid #cccccc;
-        overflow: hidden;
-        border-#{$top}: none;
-        @include flex( null,start);
-        &:first-child{
-          border-#{$top}: 1px solid #cccccc;
-        }
-        span{
-        }
-        .tit{
-          width: 90px;
-          min-width: 90px;
-          height: 100%;
-          text-align: center;
-          border-#{$right}: 1px solid #cccccc;
-          margin-#{$right}: 18px;
-        }
-        span.gitbox{
-          flex: 1;
-          height: 100%;
-          overflow: hidden;
-          @include flex(row,start);
-          a{
-            &:first-child{
-              margin-#{$right}: 30px;
-            }
-          }
-        }
-      }
-    }
-  }
-  #lineEcharts{
-    margin-#{$top}: 30px;
-    padding-#{$top}: 30px;
-    @extend %shadow;
-  }
-  #maintable{
-    margin-#{$top}: 30px;
-    padding-#{$top}: 10px;
-    @extend %shadow;
-  }
-  .lightgreen-box {
-    background-color: lightgreen;
-    height: 24px;
-  }
-  .orange-box {
-    background-color: orange;
-    height: 24px;
-  }
-  .data-box .item p span.up {
-    color: #f56c6c;
-  }
-
-  .data-box .item p span.down {
-    color: #67c23a;
-  }
+.data-box .item p span.down {
+  color: #67c23a;
+}
 </style>
