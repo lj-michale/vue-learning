273b8d92f4974f94723ac387588c650387a0cf63
Tony <economic_luojie@outlook.com>
2023-10-08 16:19:47 +0800

    feat:组件通信|setup形式下的父子组件通信|父传子

---
+++ src/components/Article.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{msg}}
+    <p> ####################### </p>
+    {{info}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+
+  props: ['msg'],
+
+  setup(props) {
+    console.log(props.msg)
+
+    return {
+      info: props.msg
+    }
+  }
+}
+</script>
--- src/views/syssetting/useFeedback.vue
+++ src/views/syssetting/useFeedback.vue
@@ -1,24 +1,22 @@
 <template>
-  {{name}}
-  <button @click="change">修改</button>
+  <div>
+    <Article :msg="name"></Article>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import Article from '@/components/Article'
 
 export default {
   name: "useFeedback",
+
+  components: {
+    Article
+  },
+
   setup() {
-    let name = ref("泪眼问花花不语，乱红飞过秋千去")
-    // let name = reactive({name: '今年花胜去年红。可惜明年花更好，知与谁同'})
-
-    function change() {
-      name.value = "人生自是有情痴，此恨不关风与月"
-    }
-
     return {
-      name,
-      change
+      name: '渐行渐远渐无书，水阔鱼沉何处问'
     }
   }
 }
